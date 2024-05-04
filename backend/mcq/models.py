0e46e6c59e01911d249dfaf772cb1d4e38a4b348
wechulisimiyu <wechu017@gmail.com>
2024-05-04 16:39:51 +0300

    remove slug

--- backend/mcq/models.py
+++ backend/mcq/models.py
@@ -1,7 +1,6 @@
 import datetime
 
 from django.db import models
-from django.utils.text import slugify
 
 from common.models import IndexedTimeStampedModel
 
@@ -9,18 +8,12 @@ from common.models import IndexedTimeStampedModel
 # unit model
 class Unit(IndexedTimeStampedModel):
     title = models.CharField(max_length=255)
-    slug = models.SlugField(blank=True)
 
     class Meta:
-        unique_together = ('title', 'slug')
+        unique_together = ('title', 'id')
 
     def __str__(self):
         return self.title
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
 
 # quiz model
 class Quiz(IndexedTimeStampedModel):
@@ -29,23 +22,19 @@ class Quiz(IndexedTimeStampedModel):
     title = models.CharField(max_length=255)
     description = models.TextField()
     unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
-    slug = models.SlugField(blank=True)
 
     # New fields, for year and tags when we bundle quizzes and create a unified qbank for a unit if someone wants it
     year_tested = models.IntegerField(('year'), choices=YEAR_CHOICES, default=datetime.datetime.now().year)
-    # tags = models.CharField(max_length=255, default=unit)  # default is the unit this quiz belongs to, in future will be topic
     paper_type = models.CharField(max_length=255, choices=[('EOR', 'End of Rotation'), ('EOY', 'End of Year')], default='EOR')
     related_topic = models.ForeignKey(Unit, related_name='related_qustions', on_delete=models.SET_NULL, null=True, blank=True)
 
     class Meta:
-        unique_together = ('unit', 'slug')
+        unique_together = ('title', 'id')
 
     def __str__(self):
         return self.title
 
     def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)
         if self.related_topic is None:
             self.related_topic = self.unit
         super().save(*args, **kwargs)
@@ -55,20 +44,17 @@ class Quiz(IndexedTimeStampedModel):
 class Question(IndexedTimeStampedModel):
     text = models.TextField()
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
-    slug = models.SlugField(blank=True)
     related_topic = models.ForeignKey(Unit, related_name='related_quizzes', on_delete=models.SET_NULL, null=True, blank=True)
 
     class Meta:
-        unique_together = ('quiz', 'slug')
+        unique_together = ('text', 'id')
 
     def __str__(self):
         return self.text
 
     def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.text)
         if self.related_topic is None:
-            self.related_topic = self.unit
+            self.related_topic = self.quiz.unit
         super().save(*args, **kwargs)
 
 # choices model
