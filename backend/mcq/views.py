745e83de9b2c28507b4e28da8df47c5386bcfc30
wechulisimiyu <wechu017@gmail.com>
2024-05-08 16:34:40 +0300

    add views for quiz management

--- backend/mcq/views.py
+++ backend/mcq/views.py
@@ -5,31 +5,47 @@ from django.views.generic import CreateView, DeleteView, DetailView, ListView, U
 from .models import Quiz
 
 
-class QuizListView(ListView):
-    model = Quiz
-    template_name = 'quiz_list.html'
+class OwnerMixin:
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner=self.request.user)
 
-class QuizCreateView(CreateView):
-    model = Quiz
-    template_name = 'quiz_new.html'
-    fields = ('title', 'description', 'unit', 'year_tested', 'tags', 'paper_type')
+class OwnerEditMixin:
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
 
-class QuizDetailView(DetailView):
+class OwnerQuizMixin(OwnerMixin):
     model = Quiz
-    template_name = 'quiz_detail.html'
-    slug_field = 'slug'
-    slug_url_kwarg = 'slug'
-
-class QuizUpdateView(UpdateView):
-    model = Quiz
-    template_name = 'quiz_edit.html'
-    fields = ('title', 'description', 'unit', 'year_tested', 'tags', 'paper_type')
-    slug_field = 'slug'
-    slug_url_kwarg = 'slug'
-
-class QuizDeleteView(DeleteView):
-    model = Quiz
-    template_name = 'quiz_delete.html'
+    fields = ['title', 'description', 'unit', 'year_tested', 'related_topic', 'paper_type', 'file']
     success_url = reverse_lazy('quiz_list')
-    slug_field = 'slug'
-    slug_url_kwarg = 'slug'
+
+class OwnerQuizEditMixin(OwnerQuizMixin, OwnerEditMixin):
+    template_name = 'mcq/dashboard/quiz/form.html'
+
+class ManageQuizView(OwnerQuizMixin, ListView):
+    template_name = 'mcq/dashboard/quiz/list.html'
+
+
+class QuizCreateView(OwnerQuizEditMixin, CreateView):
+    # model = Quiz
+    # template_name = 'mcq/dashboard/quiz/new.html'
+    # fields = ('title', 'description', 'unit', 'year_tested',
+    #           'related_topic', 'paper_type', 'file')
+    pass
+
+
+# class QuizDetailView(DetailView):
+#     model = Quiz
+#     template_name = 'quiz_detail.html'
+
+#     def get_object(self, queryset=None):
+#         return get_object_or_404(Quiz, id=self.kwargs.get('id'))
+
+
+class QuizUpdateView(OwnerQuizEditMixin, UpdateView):
+    pass
+
+
+class QuizDeleteView(OwnerQuizMixin, DeleteView):
+    template_name = 'dashboard/quiz/delete.html'
