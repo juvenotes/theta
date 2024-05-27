ccd86532bbf10e04bd577ceb535d05e7ab87ddf6
Wechuli Simiyu <wechuli017@gmail.com>
2024-05-27 22:34:31 +0300

    update settings module

--- backend/juvenotes/wsgi.py
+++ backend/juvenotes/wsgi.py
@@ -3,6 +3,10 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "juvenotes.settings.production")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "juvenotes.settings.production")
+# Check for the WEBSITE_HOSTNAME environment variable to see if we are running in Azure Ap Service
+# If so, then load the settings from production.py
+settings_module = 'juvenotes.settings.production' if 'WEBSITE_HOSTNAME' in os.environ else 'juvenotes.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
