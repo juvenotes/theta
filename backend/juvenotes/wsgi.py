9eb2ff6e195c4fb4234005025ac3c2568f797299
Wechuli Simiyu <wechuli017@gmail.com>
2024-05-31 16:48:45 +0300

    update django settings module

--- backend/juvenotes/wsgi.py
+++ backend/juvenotes/wsgi.py
@@ -3,6 +3,9 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "juvenotes.settings.production")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "juvenotes.settings.production")
+
+settings_module = 'juvenotes.settings.production' if 'WEBSITE_HOSTNAME' in os.environ else config("DJANGO_SETTINGS_MODULE", default=None)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
