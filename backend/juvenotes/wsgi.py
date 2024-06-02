efdc313f640e32457237fc0b89c11bf03a11a8cc
Wechuli Simiyu <84225314+wechulisimiyu@users.noreply.github.com>
2024-06-02 08:28:18 +0300

    Update wsgi.py

--- backend/juvenotes/wsgi.py
+++ backend/juvenotes/wsgi.py
@@ -5,7 +5,7 @@ from django.core.wsgi import get_wsgi_application
 
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "juvenotes.settings.production")
 
-settings_module = 'juvenotes.settings.production' if 'WEBSITE_HOSTNAME' in os.environ else config("DJANGO_SETTINGS_MODULE", default=None)
+settings_module = 'juvenotes.settings.production' if 'HOSTNAME' in os.environ else config("DJANGO_SETTINGS_MODULE", default=None)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
