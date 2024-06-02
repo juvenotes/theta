e99b373b7c5b67062953e71c006dfc2bc9a3ab58
Wechuli Simiyu <84225314+wechulisimiyu@users.noreply.github.com>
2024-06-02 08:54:40 +0300

    Update base.py
    Django defe

--- backend/juvenotes/settings/base.py
+++ backend/juvenotes/settings/base.py
@@ -257,7 +257,7 @@ CSP_IMG_SRC = [
 DEFENDER_LOGIN_FAILURE_LIMIT = 3
 DEFENDER_COOLOFF_TIME = 300  # 5 minutes
 DEFENDER_LOCKOUT_TEMPLATE = "defender/lockout.html"
-if os.getenv('DJANGO_SETTINGS_MODULE') != 'juvenotes.settings.production':
+if 'HOSTNAME' in os.environ:
     DEFENDER_REDIS_URL = config("AZURE_REDIS_CONNECTIONSTRING")
 else:
     DEFENDER_REDIS_URL = config("REDIS_URL")
