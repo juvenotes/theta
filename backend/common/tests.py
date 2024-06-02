685f735ab7aec9ad78d78120f8081fc39d044322
Wechuli Simiyu <wechuli017@gmail.com>
2024-06-02 17:00:54 +0300

    attempt test to check django settings module

--- backend/common/tests.py
+++ backend/common/tests.py
@@ -1,3 +1,10 @@
+# import os
+
+# from django.conf import settings
+# from django.test import TestCase
+
+# from decouple import config
+
 from common.utils.tests import TestCaseUtils
 
 
@@ -7,3 +14,10 @@ class TestIndexView(TestCaseUtils):
     def test_returns_status_200(self):
         response = self.auth_client.get(self.reverse(self.view_name))
         self.assertResponse200(response)
+
+# class SettingsTest(TestCase):
+#     def test_django_settings_module(self):
+#         if os.getenv('WEBSITE_HOSTNAME'):
+#             self.assertEqual(settings.SETTINGS_MODULE, 'juvenotes.settings.production')
+#         else:
+#             self.assertEqual(settings.SETTINGS_MODULE, config("DJANGO_SETTINGS_MODULE"))
