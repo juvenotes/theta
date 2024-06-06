bd95dabd2d600921106f82737a0089f259fb433b
Wechuli Simiyu <wechuli017@gmail.com>
2024-06-06 10:35:36 +0300

    fix 'list' object has no attribute 'lower'

--- backend/juvenotes/settings/production.py
+++ backend/juvenotes/settings/production.py
@@ -8,7 +8,7 @@ from sentry_sdk.integrations.django import DjangoIntegration
 from .base import *
 
 
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME'],os.environ['CUSTOM_HOSTNAME']], ['.juvenotes.com']
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME'], os.environ['CUSTOM_HOSTNAME'], '.juvenotes.com']
 # CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 # CSRF_TRUSTED_ORIGINS += ['.juvenotes.com']
 CSRF_TRUSTED_ORIGINS = ['https://juvenotes.azurewebsites.net', 'https://*.juvenotes.com']
