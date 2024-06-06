a01ae08183827dc0f8743659ebe39873f13315a5
Wechuli Simiyu <wechuli017@gmail.com>
2024-06-06 10:21:32 +0300

    fix backend_user_changelist and deprecated actions

--- backend/juvenotes/settings/base.py
+++ backend/juvenotes/settings/base.py
@@ -132,28 +132,28 @@ REST_FRAMEWORK = {
 UNFOLD = {
     "SITE_HEADER": _("Juvenotes Admin Site - Manage Resources"),
     "SITE_TITLE": _("Juvenotes Admin Site"),
-    "SIDEBAR": {
-        "show_search": True,
-        "show_all_applications": True,
-        "navigation": [
-            {
-                "title": _("Navigation"),
-                "separator": False,
-                "items": [
-                    {
-                        "title": _("Users"),
-                        "icon": "person",
-                        "link": reverse_lazy("admin:backend_user_changelist"),
-                    },
-                    {
-                        "title": _("Groups"),
-                        "icon": "label",
-                        "link": reverse_lazy("admin:auth_group_changelist"),
-                    },
-                ],
-            },
-        ],
-    },
+    # "SIDEBAR": {
+    #     "show_search": True,
+    #     "show_all_applications": True,
+    #     "navigation": [
+    #         {
+    #             "title": _("Navigation"),
+    #             "separator": False,
+    #             "items": [
+    #                 {
+    #                     "title": _("Users"),
+    #                     "icon": "person",
+    #                     "link": reverse_lazy("admin:backend_user_changelist"),
+    #                 },
+    #                 {
+    #                     "title": _("Groups"),
+    #                     "icon": "label",
+    #                     "link": reverse_lazy("admin:auth_group_changelist"),
+    #                 },
+    #             ],
+    #         },
+    #     ],
+    # },
 }
 
 # drf-spectacular
