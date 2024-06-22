ced55373d0d804f5915abe3537f102249a856eaa
emmanuelkorir <emmanuelqkorir@gmail.com>
2024-06-23 00:17:03 +0300

    add YearOfStudy, Subject, and ShortAnswerQuestion models in admin.py

--- backend/saq/admin.py
+++ backend/saq/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import YearOfStudy, Subject, ShortAnswerQuestion 
 
-# Register your models here.
+admin.site.register(YearOfStudy)
+admin.site.register(Subject)
+admin.site.register(ShortAnswerQuestion)
+
